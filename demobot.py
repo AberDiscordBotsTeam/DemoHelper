6c70de830562a2ec697981b17f6e6823564567b3
joa38 <joa38@aber.ac.uk>
2020-08-12 20:47:09 +0100

    Removed error catching

--- demobot.py
+++ demobot.py
@@ -45,22 +45,5 @@ async def on_ready():
     """
     print(f'{bot.user.name} has connected to Discord!')
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    """
-    Handle the Error message in a nice way.
-    """
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('You do not have the correct role for this command.')
-    elif isinstance(error, commands.errors.MissingRequiredArgument):
-        await ctx.send('You are missing a required argument.')
-    elif isinstance(error, commands.errors.CommandNotFound):
-        await ctx.send('Sorry, I didn\'t get that.')
-    else:
-        await ctx.send('Something went wrong, please contact an Admin.')
-        logging.error(error)
-
-
 # Start the bot
 bot.run(TOKEN)
