abebe2b29bf1346b0d57034ffb6022ce77249151
JoelLucaAdams <57064123+JoelLucaAdams@users.noreply.github.com>
2020-10-24 01:38:25 +0100

    Fixed ping feature

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -97,6 +97,5 @@ You can also set the `help-1` role to hide channel history so the student can't 
         start_time = time.time()
         message = await ctx.send('pong. `DWSP latency: ' + str(round(ctx.bot.latency * 1000)) + 'ms`')
         end_time = time.time()
-        await message.edit(content='pong. `DWSP latency: ' + str(round(ctx.bot.latency * 1000)) + 'ms` '
-                                                                                                  '`Response time: ' + str(
-            round(end_time - start_time, 3)) + 'ms`')
+        await message.edit(content='pong. `DWSP latency: ' + str(round(ctx.bot.latency * 1000)) + 'ms` ' +
+                                   '`Response time: ' + str(int((end_time - start_time) * 1000) + 'ms`')
