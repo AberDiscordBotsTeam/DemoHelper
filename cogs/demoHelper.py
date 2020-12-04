763307552a1fa54bb4e2d982565d6766c90bd2be
joa38 <joa38@aber.ac.uk>
2020-12-04 16:16:09 +0000

    moved printList command to file

--- cogs/demoHelper.py
+++ cogs/demoHelper.py
@@ -7,7 +7,6 @@ import logging
 import shelve
 
 from cogs import adminRoles, addMessageFile
-from helpers import listPrint
 
 queues = {'dummy': []}
 
@@ -65,6 +64,18 @@ def getQueue(serverName: str):
     else:
         queues[serverName] = []
         return queues.get(serverName)
+
+
+def listPrint(list):
+    """
+    Prints out a list in a less developery way.
+    :param list: the list to print
+    :return: the generated string from the list.
+    """
+    out = ''
+    for l in list:
+        out = out + str(l) + ', '
+    return out[:-2]
 
 
 def getCustomAddMessage(serverName: str):
