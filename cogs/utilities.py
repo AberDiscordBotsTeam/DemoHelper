ba0718b362415b3ad71f1c9ba6771a658d15c2dc
joa38 <joa38@aber.ac.uk>
2021-01-29 13:16:01 +0000

    Added icon to ping command

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -99,5 +99,5 @@ class Utilities(commands.Cog):
         start_time = time.time()
         message = await ctx.send('pong. `DWSP latency: ' + str(round(ctx.bot.latency * 1000)) + 'ms`')
         end_time = time.time()
-        await message.edit(content='pong. `DWSP latency: ' + str(round(ctx.bot.latency * 1000)) + 'ms` ' +
+        await message.edit(content='🏓 pong. `DWSP latency: ' + str(round(ctx.bot.latency * 1000)) + 'ms` ' +
                                    '`Response time: ' + str(int((end_time - start_time) * 1000)) + 'ms`')
